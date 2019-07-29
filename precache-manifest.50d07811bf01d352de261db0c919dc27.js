bae6b9c43253e588e4059879764bd2fc98605582
Mark John Hizon <hizonmark3@google.com>
2019-07-29 17:22:36 +0800

    Updates

---
+++ precache-manifest.50d07811bf01d352de261db0c919dc27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78268e9f913d86eb58425e27c67831a2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "404e9b1cd857e187a028",
+    "url": "/monsters-rolodex/static/css/main.e89faef0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "404e9b1cd857e187a028",
+    "url": "/monsters-rolodex/static/js/main.5d0b8b26.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
